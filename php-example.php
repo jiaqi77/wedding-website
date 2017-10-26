65769d8c08c987687150a3f32299d725dcea0e94
Jiaqi Ma (C) <CZR2SG@NAM.corp.gm.com>
2017-10-25 21:56:19 -0400

    submit

--- php-example.php
+++ php-example.php
@@ -2,7 +2,7 @@
   // The global $_POST variable allows you to access the data sent with the POST method by name
   // To access the data sent with the GET method, you can use $_GET
   $name = htmlspecialchars($_POST['name']);
-  $guest  = htmlspecialchars($_POST['Guest']);
+  $guest  = htmlspecialchars($_POST['guest']);
 
   echo  $name, ' ', $guest;
 ?>
